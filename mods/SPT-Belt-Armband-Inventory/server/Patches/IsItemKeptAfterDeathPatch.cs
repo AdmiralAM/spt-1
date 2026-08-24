cfb5d177e55aa732985c8a9b659bff8704b713f7
AdmiralAM <amanol311@gmail.com>
2026-08-24 03:00:41 +0300

    Belt R8: keep belt tree after death

---
+++ mods/SPT-Belt-Armband-Inventory/server/Patches/IsItemKeptAfterDeathPatch.cs
@@ -0,0 +1,31 @@
+using System.Reflection;
+using SPTarkov.Reflection.Patching;
+using SPTarkov.Server.Core.Helpers.InRaid;
+using SPTarkov.Server.Core.Models.Eft.Common;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+
+namespace SPTBeltArmbandInventory.Server.Patches;
+
+public sealed class IsItemKeptAfterDeathPatch : AbstractPatch
+{
+    protected override MethodBase? GetTargetMethod()
+    {
+        return typeof(InRaidHelper).GetMethod(
+            "IsItemKeptAfterDeath",
+            BindingFlags.NonPublic | BindingFlags.Instance);
+    }
+
+    [PatchPostfix]
+    public static void Postfix(PmcData pmcData, Item itemToCheck, ref bool __result)
+    {
+        if (__result || pmcData.Inventory?.Items is null) return;
+
+        var nodes = pmcData.Inventory.Items.Select(item => new BeltInventoryNode(
+            item.Id.ToString(),
+            item.ParentId?.ToString(),
+            item.SlotId));
+
+        if (BeltDeathPolicy.ShouldKeep(itemToCheck.Id.ToString(), nodes))
+            __result = true;
+    }
+}
