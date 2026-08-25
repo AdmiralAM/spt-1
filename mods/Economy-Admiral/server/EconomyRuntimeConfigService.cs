4b79b5374a371dea42d43aeb3ad27a12c23e22a3
Admiral <amanol311@gmail.com>
2026-08-26 02:16:17 +0300

    Economy Admiral: consume DI-registered config singleton

--- mods/Economy-Admiral/server/EconomyRuntimeConfigService.cs
+++ mods/Economy-Admiral/server/EconomyRuntimeConfigService.cs
@@ -1,29 +1,13 @@
-using Path = System.IO.Path;
 using SPTarkov.DI.Annotations;
-using SPTarkov.Server.Core.Helpers.Server;
 
 namespace SPTEconomy;
 
 [Injectable]
-public sealed class EconomyRuntimeConfigService(ModHelper modHelper)
+public sealed class EconomyRuntimeConfigService(EconomyConfig config)
 {
-    private EconomyConfig? cached;
-
-    public async Task<EconomyConfig> GetAsync(CancellationToken cancellationToken)
+    public Task<EconomyConfig> GetAsync(CancellationToken cancellationToken)
     {
-        if (cached is not null)
-        {
-            return cached;
-        }
-
-        var modPath = modHelper.GetAbsolutePathToModFolder(typeof(EconomyRuntimeConfigService).Assembly);
-        var configPath = Path.Combine(modPath, "config", "config.json");
-        var config = File.Exists(configPath)
-            ? EconomyConfigJsonLoader.Deserialize(await File.ReadAllTextAsync(configPath, cancellationToken))
-            : new EconomyConfig();
-
-        EconomyConfigValidator.Validate(config);
-        cached = config;
-        return cached;
+        cancellationToken.ThrowIfCancellationRequested();
+        return Task.FromResult(config);
     }
 }
