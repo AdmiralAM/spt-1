839dbaeef0942fc5d9626736101ce358c5034f16
Admiral <amanol311@gmail.com>
2026-08-25 21:40:19 +0300

    refactor(economy-admiral): rename module namespace

--- mods/Economy-Admiral/server/EconomyMod.cs
+++ mods/Economy-Admiral/server/EconomyMod.cs
@@ -2,7 +2,7 @@ using JetBrains.Annotations;
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
 
-namespace SPTEconomy;
+namespace EconomyAdmiral;
 
 [Injectable(TypePriority = OnLoadOrder.PostLoad + 1000), UsedImplicitly]
 public sealed class EconomyMod(EconomyAuditService auditService) : IOnLoad
