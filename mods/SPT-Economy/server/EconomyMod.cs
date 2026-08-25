5addc2ff1b356f8224686b130b95211d7c164217
Admiral <amanol311@gmail.com>
2026-08-25 21:20:08 +0300

    feat(economy): run audit at SPT post-load boundary

---
+++ mods/SPT-Economy/server/EconomyMod.cs
@@ -0,0 +1,14 @@
+using JetBrains.Annotations;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+
+namespace SPTEconomy;
+
+[Injectable(TypePriority = OnLoadOrder.PostLoad + 1000), UsedImplicitly]
+public sealed class EconomyMod(EconomyAuditService auditService) : IOnLoad
+{
+    public Task OnLoadAsync(CancellationToken cancellationToken)
+    {
+        return auditService.RunAsync(cancellationToken);
+    }
+}
