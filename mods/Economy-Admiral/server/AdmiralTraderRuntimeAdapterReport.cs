4d794afb975712cf7d755a837f1093bd17456565
Admiral <amanol311@gmail.com>
2026-08-27 00:16:33 +0300

    Economy Admiral: model degraded Admiral Trader adapter state

--- mods/Economy-Admiral/server/AdmiralTraderRuntimeAdapterReport.cs
+++ mods/Economy-Admiral/server/AdmiralTraderRuntimeAdapterReport.cs
@@ -2,8 +2,11 @@ namespace SPTEconomy;
 
 public sealed record AdmiralTraderRuntimeAdapterReport
 {
-    public int SchemaVersion { get; init; } = 1;
+    public int SchemaVersion { get; init; } = 2;
     public required bool Installed { get; init; }
+    public required bool ContractAvailable { get; init; }
+    public required string ContractState { get; init; }
+    public string? ContractDiagnostic { get; init; }
     public required string ModGuid { get; init; }
     public required string AttributionConfidence { get; init; }
     public int OfferCount { get; init; }
