296bee2ee104a2719a2cf319e6e555d6a92bc9c0
Admiral <amanol311@gmail.com>
2026-08-26 22:23:49 +0300

    Economy Admiral: use shared adapter report contract

--- mods/Economy-Admiral/server/AdmiralTraderRuntimeAdapterService.cs
+++ mods/Economy-Admiral/server/AdmiralTraderRuntimeAdapterService.cs
@@ -5,19 +5,6 @@ using SPTarkov.Server.Core.Helpers.Server;
 
 namespace SPTEconomy;
 
-public sealed record AdmiralTraderRuntimeAdapterReport
-{
-    public int SchemaVersion { get; init; } = 1;
-    public required bool Installed { get; init; }
-    public required string ModGuid { get; init; }
-    public required string AttributionConfidence { get; init; }
-    public int OfferCount { get; init; }
-    public int BoundedRenewableOfferCount { get; init; }
-    public int? MinimumEffectiveProgressionLevel { get; init; }
-    public int? MaximumEffectiveProgressionLevel { get; init; }
-    public required IReadOnlyList<AdmiralTraderOfferAdapterEvidence> Offers { get; init; }
-}
-
 [Injectable]
 public sealed class AdmiralTraderRuntimeAdapterService(ModHelper modHelper)
 {
