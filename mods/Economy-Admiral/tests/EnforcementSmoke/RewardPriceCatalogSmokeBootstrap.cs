7eadca739a3e94516cf8deb77a11cd608fd7d08f
Admiral <amanol311@gmail.com>
2026-08-31 06:03:27 +0300

    Economy Admiral: initialize handbook prices for grouped reward smoke

---
+++ mods/Economy-Admiral/tests/EnforcementSmoke/RewardPriceCatalogSmokeBootstrap.cs
@@ -0,0 +1,17 @@
+using System.Runtime.CompilerServices;
+using SPTEconomy;
+
+internal static class RewardPriceCatalogSmokeBootstrap
+{
+    [ModuleInitializer]
+    internal static void Initialize()
+    {
+        QuestRewardHandbookPriceCatalog.Initialize(new Dictionary<string, double>(StringComparer.Ordinal)
+        {
+            ["same-tpl"] = 100d,
+            ["tpl-a"] = 100d,
+            ["tpl-b"] = 100d,
+            ["tpl-c"] = 100d,
+        });
+    }
+}
