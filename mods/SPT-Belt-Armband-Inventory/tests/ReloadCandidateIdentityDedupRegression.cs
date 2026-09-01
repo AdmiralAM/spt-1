e4c90058a209dd3a3e1035f719b3be59c0d0fe69
Admiral <amanol311@gmail.com>
2026-09-01 17:00:24 +0300

    Align reload identity regression with exact slot15 contract

--- mods/SPT-Belt-Armband-Inventory/tests/ReloadCandidateIdentityDedupRegression.cs
+++ mods/SPT-Belt-Armband-Inventory/tests/ReloadCandidateIdentityDedupRegression.cs
@@ -32,7 +32,7 @@ internal static class ReloadCandidateIdentityDedupRegression
         ReloadCandidateBridgeRuntime.Reset();
         ReloadCandidateBridgeRuntime.GetItemsInSlots = typeof(FakeInventory).GetMethod(nameof(FakeInventory.GetItemsInSlots))
             ?? throw new InvalidOperationException("Reload candidate identity regression failed: fake GetItemsInSlots missing");
-        ReloadCandidateBridgeRuntime.BeltSlotsArgument = new object();
+        ReloadCandidateBridgeRuntime.BeltSlotsArgument = new[] { RuntimeIdentity.DedicatedBeltEquipmentSlotValue };
         ReloadCandidateBridgeRuntime.OriginalFastAccessSlots = recognizedSlots;
         ReloadCandidateBridgeRuntime.ItemType = typeof(FakeItem);
         ReloadCandidateBridgeRuntime.MagazineType = typeof(FakeMagazine);
